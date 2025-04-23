fbe9a5ce991259e75af7fd4576aa3b52304d85c6
lakshmi durga <lakshmidurga.b@worthitcs.com>
2025-04-23 09:29:49 +0530

    22-04-2025

---
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,49 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Model.User;
+import com.example.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id)
+    {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers()
+    {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+    // Create and Save User
+    @PostMapping("/save-user")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        User savedUser = userService.saveuser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    // Update User
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return ResponseEntity.ok(userService.updateUser(id, userDetails));
+    }
+
+    // Delete User
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
